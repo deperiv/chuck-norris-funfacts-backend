1ea06b08d28498d55b3dc56a55558541a836bfec
deperazar <danielpblog2014@gmail.com>
2022-02-18 08:00:33 -0500

    Fully functionality in adding/removing favorite facts in DB from Front-End

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const axios = require('axios');
-// const knex = require('knex');
+const knex = require('knex');
+const { chuckDB } = require('pg/lib/defaults');
+const { json } = require('express/lib/response');
 
-// const { database } = require('pg/lib/defaults');
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'perazarivera1998',
+      database : 'chuckDB'
+    }
+});
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
@@ -67,17 +78,6 @@ const database = {
     ]
 }
 
-// const db = knex({
-//     client: 'pg',
-//     connection: {
-//       host : '127.0.0.1',
-//       port : 5432,
-//       user : 'postgres',
-//       password : 'perazarivera1998',
-//       database : 'chuckDB'
-//     }
-// });
-
 //Get users
 app.get('/', (req, res) => {
     res.send(database.users);
@@ -121,28 +121,86 @@ app.post('/register', (req, res) => {
         joined: new Date()
     }
     database.users.push(newUser)
-    res.json(database.users[database.users.length-1]); //res.json() is almost equal to res.send()
+    res.json(database.users[database.users.length-1]); 
 })
 
-app.get('/getFavorites', (req, res) => {
-    // const { userID } = req.params;
-    res.json(database.users[0].favoriteFacts); //res.json() is almost equal to res.send()
+app.get('/getFavorites/:userid', (req, res) => {
+    const {userid} = req.params;
+    db.select('facts.factid', 'facts.facttext')
+    .from('users')
+    .where('users.id', userid) // Remove in future
+    .joinRaw('left join facts on facts.factid = any (users.favoritefacts)')
+    .then(favoritefacts => {
+        const formatedResp = favoritefacts.map(fact => {
+            return {
+                id: fact.factid,
+                value: fact.facttext 
+            }
+        })
+        res.json(formatedResp)
+    })
+    .catch(err => res.status(400).json('Unable to get account'));
 })
-
 
 app.post('/addFavorite', (req, res) => {
-    // const { userID } = req.params;
-    const { fact } = req.body;
-    database.users[0].favoriteFacts.unshift(fact)
-    res.json(database.users[0].favoriteFacts); //res.json() is almost equal to res.send()
+    const { factid, userid, text } = req.body;
+    db.select('facts.facttext')
+    .from('facts')
+    .where('factid','=',factid)
+    .then(data => {
+        if (data.length) { // The fact is already in the FACTS TABLE
+            db('facts')
+            .where('factid', factid)
+            .update({
+                users: db.raw('array_append(users, ?)', [userid])
+            })
+            .catch(err => res.status(400).json('Unable to update the FACTS table'));
+        } else { // The fact is not in the FACTS TABLE
+            db('facts')
+            .insert({
+                factid: factid,
+                users: [userid],
+                facttext: text
+            })
+            .catch(err => res.status(400).json('Unable to create a new Fact'));
+        }
+        db('users')
+        .where('id', userid)
+        .update({
+            favoritefacts: db.raw('array_append(favoritefacts, ?)', [factid])
+        })
+        .then(resp => res.json(resp))
+        .catch(err => res.status(400).json('Unable to update the USERS table'));
+    })
 })
 
 app.delete('/removeFavorite', (req, res) => {
-    // const { userID } = req.params;
-    const { id } = req.body;
-    let facts = database.users[0].favoriteFacts.filter(fact => fact.id !== id)
-    database.users[0].favoriteFacts = facts
-    res.json(database.users[0].favoriteFacts); //res.json() is almost equal to res.send()
+    const { factid, userid } = req.body;
+    db.select('facts.users')
+    .from('facts')
+    .where('factid', factid)
+    .then(data => {
+        if (data[0].users.length === 1) { // The fact has only one user
+            db('facts')
+            .where('factid', factid)
+            .del()
+            .catch(err => res.status(400).json(`Unable to delete: ${err}`));
+        } else { // The fact has more than one user
+            db('facts')
+            .where('factid', factid)
+            .update({
+                users: db.raw('array_remove(users, ?)', [userid])
+            })
+            .catch(err => res.status(400).json('Unable to update the FACTS table'));
+        }
+        db('users')
+        .where('id', userid)
+        .update({
+            favoritefacts: db.raw('array_remove(favoritefacts, ?)', [factid])
+        })
+        .then(resp => res.json(resp))
+        .catch(err => res.status(400).json('Unable to update the USERS table'));
+    })
 })
 
 
